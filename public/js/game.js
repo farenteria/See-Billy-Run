2b605e14017bcbd45d5987ec4a4e33bc0464815e
Fernando Renteria <rentfern@gmail.com>
2015-07-11 12:12:32 -0500

    Added a scrolling background using placeholder image. This will be replaced at some point in the future

--- public/js/game.js
+++ public/js/game.js
@@ -1,16 +1,38 @@
 "use strict";
 
 (function(){
-	init();
+	var container;
+	var renderer;
+	var backSprite;
 
 	function init(){
-		var container = new PIXI.Container(); //our stage
+		var backTexture;
+
+		container = new PIXI.Container(); //our stage
 		//will either use canvas or webgl, depending on browser
-		var renderer = PIXI.autoDetectRenderer(512, 384, {
+		renderer = PIXI.autoDetectRenderer(512, 384, {
 			view:document.getElementById("game-canvas")
 		});
 
-		//we finally render our stage according to render
-		renderer.render(container);
+		backTexture = PIXI.Texture.fromImage("img/bg-far.png");
+		backSprite = new PIXI.extras.TilingSprite(backTexture, 512, 384);
+		backSprite.position.x = 0;
+		backSprite.position.y = 0;
+		backSprite.tilePosition.x = 0;
+		backSprite.tilePosition.y = 0;
+		container.addChild(backSprite); //adds background to container
+
+		//initial update
+		requestAnimationFrame(update);
 	}
+
+	//moves background to the left a bit, and renders every frame
+	function update(){
+		backSprite.tilePosition.x -= 0.5;
+
+		renderer.render(container); //renders container
+		requestAnimationFrame(update); //updates every frame
+	}
+
+	init();
 })();
