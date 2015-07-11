668d834a46f4607061fb1d327c880d2e1a7f0566
Fernando Renteria <rentfern@gmail.com>
2015-07-11 11:03:45 -0500

    Added some basic render statements to our game.js according to pixi.js library, and removed background coloring on style.css as it was messing with the background of canvas

--- public/js/game.js
+++ public/js/game.js
@@ -4,6 +4,13 @@
 	init();
 
 	function init(){
-		console.log("YOLO");
+		var container = new PIXI.Container(); //our stage
+		//will either use canvas or webgl, depending on browser
+		var renderer = PIXI.autoDetectRenderer(512, 384, {
+			view:document.getElementById("game-canvas")
+		});
+
+		//we finally render our stage according to render
+		renderer.render(container);
 	}
 })();
