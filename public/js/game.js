3be7799b99f9ee3fb2b6fb2ff3277b0e0b0ef0fa
Fernando Renteria <rentfern@gmail.com>
2015-07-21 09:28:34 -0500

    Moved our character logic from game.js to character.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,7 @@
+/*
+	Handles what goes into our game screen, and how users interact with
+	game
+*/
 "use strict";
 
 (function(){
@@ -58,55 +62,6 @@
 				console.log("down");	
 			}
 		});
-	}
-
-	//jumping animation for character
-	function makeCharacterJump(){
-		$('#stick-figure').animate({
-			top: '-=40'
-		}, 300).animate({
-			top: '+=40'
-		}, 300);
-
-		// add jump class
-		animateWithClass('add', 'jump');
-
-		//remove jump class for landing
-		setTimeout(function(){
-			animateWithClass('remove', 'jump');
-		}, 300);
-	}
-
-	/*
-		Will either add class (for action animation), or remove class
-		(to return character back to default position).
-		'addOrRemove' refers to whether we are adding a class or removing it.
-		'action' refers to what the character will be doing so we can use 
-		the appropriate ids and classes
-	*/
-	function animateWithClass(addOrRemove, action){
-		var limbs = ['arm', 'leg'];
-
-		for(var i = 0; i < limbs.length; i++){
-			for(var j = 0; j < limbs.length; j++){
-				if(addOrRemove == 'add'){
-					addLimbClass(limbs[i], action, j.toString());
-				}else if(addOrRemove =='remove'){
-					removeLimbClass(limbs[i], action, j.toString());
-					$('#' + limbs[i] + '-' + j).animate({
-						transition: '.3s'
-					});
-				}
-			}
-		}	
-	}
-
-	function addLimbClass(limb, action, limbNum){
-		$('#' + limb + '-' + limbNum).addClass(limb + '-' + limbNum + '-' + action);
-	}
-
-	function removeLimbClass(limb, action, limbNum){
-		$('#' + limb + '-' + limbNum).removeClass(limb + '-' + limbNum + '-' + action);
 	}
 
 	init();
